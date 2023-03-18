fe5e928395ee2cb5994a402770e533cc29f5b762
Rishabh Dixit <87074249+1-bit-1@users.noreply.github.com>
2023-03-18 22:02:27 +0530

    Updates

---
+++ precache-manifest.b4b6911e330d331109f8a22ce97c6b8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dbe32f4eea060a3d75a",
+    "url": "https//1-bit-1.github.io/trello-board/static/js/main.6dbe32f4.chunk.js"
+  },
+  {
+    "revision": "d8f5ba55c3b78c25f0a5",
+    "url": "https//1-bit-1.github.io/trello-board/static/js/1.d8f5ba55.chunk.js"
+  },
+  {
+    "revision": "9089ceace9d77fe65073",
+    "url": "https//1-bit-1.github.io/trello-board/static/js/2.9089ceac.chunk.js"
+  },
+  {
+    "revision": "482d733a5f8d05d01a49",
+    "url": "https//1-bit-1.github.io/trello-board/static/js/runtime~main.482d733a.js"
+  },
+  {
+    "revision": "4f6514a0bbe510e2925dd9f3dbe26596",
+    "url": "https//1-bit-1.github.io/trello-board/index.html"
+  }
+];
